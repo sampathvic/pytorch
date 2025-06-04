e2774949561748851a6ccbcf65fd32ffd9f0dfbe
Catherine Lee <csl@fb.com>
2025-06-04 09:16:00 -0700

    tc

--- .github/scripts/lintrunner.sh
+++ .github/scripts/lintrunner.sh
@@ -52,7 +52,7 @@ jq --raw-output \
     '"::\(if .severity == "advice" or .severity == "disabled" then "warning" else .severity end) file=\(.path),line=\(.line),col=\(.char),title=\(.code) \(.name)::" + (.description | gsub("\\n"; "%0A"))' \
     lint.json || true
 
-python -m tools.linter.commit_changes --mode format-output --input-file lint.json
+python -m tools.linter.commit_changes --mode format-input --input-file lint.json
 mv lint.json.formatted "${RUNNER_ARTIFACT_DIR}/"
 
 exit $RC
