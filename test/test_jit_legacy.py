edf4187f963a87452c97dc7961e7475e7d1aa7eb
Anthony Barbier <anthonyb@graphcore.ai>
2025-06-25 13:39:09 +0100

    Fix test_jit_legacy test

--- test/test_jit_legacy.py
+++ test/test_jit_legacy.py
@@ -2,7 +2,13 @@
 
 import sys
 sys.argv.append("--jit-executor=legacy")
-from test_jit import *  # noqa: F403
+from torch.testing._internal.common_utils import parse_cmd_line_args, run_tests
+
+# The tests decorators depend on command line arguments
+if __name__ == '__main__':
+    parse_cmd_line_args()
+
+from test_jit import *  # noqa: F403, F401
 
 if __name__ == '__main__':
     run_tests()
