36da9a91574b2a7068779498444f0cf98d161400
Anthony Barbier <anthonyb@graphcore.ai>
2025-06-26 15:11:13 +0100

    Fix jit tests

--- test/test_jit_legacy.py
+++ test/test_jit_legacy.py
@@ -2,7 +2,12 @@
 
 import sys
 sys.argv.append("--jit-executor=legacy")
-from torch.testing._internal.common_utils import run_tests
+from torch.testing._internal.common_utils import parse_cmd_line_args, run_tests
+
+if __name__ == '__main__':
+    # The value of GRAPH_EXECUTOR depends on command line arguments so make sure they're parsed
+    # before instantiating tests.
+    parse_cmd_line_args()
 
 from test_jit import *  # noqa: F403, F401
 
