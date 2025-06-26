c1e9d29f0c40f0ef15a41814826f939f0b06ffb6
rzou <zou3519@gmail.com>
2025-06-26 08:12:35 -0700

    Update base for Update on "[aotd] Support mutations of the same input in fw and bw"
    Original issue: https://github.com/pytorch/pytorch/issues/154820
    
    The issue happens when there is a mutation for the same input in forward AND in backward.
    
    AOTD emited copy_ after joint_function tracing. This made this fx-node to correspond to the side effects of both mutations (in forward and in backward). 
    After that partitioner can put it either in forward or in backward.
    
    
    
    The fix:
    
    1/ Introduce joint_function.handle that allows to set "post_forward" callback, to be able to check inputs state after forward
    
    We do not want to apply the mutation after joint, if we already applied it in forward. For that we need "mutation_counter" and memorize the version of mutation that we applied for  forward mutation.
    
    2/ Exposing mutation_counter to python
    
    We want to keep invariant that copy_ exist only in the end of joint graph.
    
    3/ We memorize mutation_counter and state of the inputs after forward, using the handle post_forward.
    Emit post_forward mutations after joint graph fully traced.
    
    add for post_forward mutations "must_be_in_forward" tag (similar to existing "must_be_in_backward") to keep them in forward.
    
    
    4/ Ban recompute of the source of mutation. Recompute can apply the same op (e.g. add) in forward and backward.
    For this set MUST_SAVE for the source of mutation in forward.
    
    
    
    proxy_tensor changes:
    
    By default proxy tensor updates tensor_tracker. In this case applied mutations will be chained.
    But we want that this copy_ will be independent and applied just to primals.
    For this introducing a contextmanager to be able to disable update of tensor_tracker for adding forward mutations.
    
    
    
    
    cc ezyang SherlockNoMad EikanWang jgong5 wenzhe-nrv voznesenskym penguinwu Guobing-Chen XiaobingSuper zhuhaozhe blzheng jiayisunx chenyang78 kadeng chauhang amjames
    
    [ghstack-poisoned]

--- test/distributed/test_nccl.py
+++ test/distributed/test_nccl.py
@@ -18,7 +18,6 @@ from torch.testing._internal.common_utils import (
     IS_WINDOWS,
     load_tests,
     NoTest,
-    requires_cuda_p2p_access,
     run_tests,
     skip_but_pass_in_sandcastle_if,
     TEST_WITH_ROCM,
@@ -242,17 +241,24 @@ class TestNCCL(TestCase):
             self.assertEqual(outputs[i], expected[i])
 
 
-@requires_cuda_p2p_access()
+device_type = "cuda"
+device_module = torch.get_device_module(device_type)
+
+
 class NCCLSymmetricMemoryTest(MultiProcContinousTest):
+    def _init_device(self) -> None:
+        # TODO: relieve this (seems to hang if without)
+        device_module.set_device(self.device)
+
     @property
     def device(self) -> torch.device:
-        return torch.device("cuda", self.rank)
+        return torch.device(device_type, self.rank)
 
     # To run this test, one needs to TORCH_SYMMMEM=NCCL when running the test.
     @skip_but_pass_in_sandcastle_if(TEST_WITH_ROCM, "Skip NCCL tests for ROCm")
     @skip_but_pass_in_sandcastle_if(IS_WINDOWS, "NCCL doesn't support Windows")
     def test_nccl_symmem_alloc(self):
-        torch.cuda.set_device(self.rank)
+        self._init_device()
         c10d.all_reduce(torch.ones(1, device=self.device))
         group_name = c10d.group.WORLD.group_name
         symm_mem.enable_symm_mem_for_group(group_name)
