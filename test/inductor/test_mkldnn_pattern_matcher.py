a00c4223ce3e6af722754e9aaf67388a1345808e
Jiang, Yanbing <yanbing.jiang@intel.com>
2025-07-01 08:14:57 +0000

    allow to use tf32 as fp32 internal precision for mkldnn conv/matmul/rnn
    ghstack-source-id: b384bd4685529236bc1f496496c0fcf0f226fe89
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/156802

--- test/inductor/test_mkldnn_pattern_matcher.py
+++ test/inductor/test_mkldnn_pattern_matcher.py
@@ -18,7 +18,7 @@ from torch._inductor.utils import (
 from torch.ao.quantization.quantizer.x86_inductor_quantizer import X86InductorQuantizer
 from torch.nn import functional as F
 from torch.testing._internal.common_device_type import instantiate_device_type_tests
-from torch.testing._internal.common_mkldnn import bf32_on_and_off
+from torch.testing._internal.common_mkldnn import bf32_on_and_off, tf32_on_and_off
 from torch.testing._internal.common_quantization import (
     _generate_qdq_quantized_model,
     skipIfNoDynamoSupport,
@@ -312,6 +312,8 @@ class TestPatternMatcherGeneric(TestPatternMatcherBase):
             memory_format,
             dtype,
         ) in options:
+            if dtype != torch.float32 and torch.backends.mkldnn.matmul.fp32_precision == "tf32":
+                continue
             metrics.reset()
             if dim == 4:
                 x_shape = (1, 3, 56, 56)
@@ -351,6 +353,7 @@ class TestPatternMatcherGeneric(TestPatternMatcherBase):
     @skipIfNoONEDNN
     @skipIfRocm
     @bf32_on_and_off()
+    @tf32_on_and_off()
     def test_conv2d_unary(self, device):
         self.device = device
         self._test_conv_unary_base(dim=4)
@@ -359,6 +362,7 @@ class TestPatternMatcherGeneric(TestPatternMatcherBase):
     @skipIfNoONEDNN
     @skipIfRocm
     @bf32_on_and_off()
+    @tf32_on_and_off()
     def test_conv3d_unary(self, device):
         self.device = device
         self._test_conv_unary_base(dim=5)
@@ -544,6 +548,7 @@ class TestPatternMatcherGeneric(TestPatternMatcherBase):
     @skipIfNoONEDNN
     @skipIfRocm
     @bf32_on_and_off(0.02)
+    @tf32_on_and_off()
     def test_conv2d_binary(self, device):
         self.device = device
         self._test_conv_binary_base(dim=4)
@@ -552,6 +557,7 @@ class TestPatternMatcherGeneric(TestPatternMatcherBase):
     @skipIfNoONEDNN
     @skipIfRocm
     @bf32_on_and_off(0.02)
+    @tf32_on_and_off()
     def test_conv3d_binary(self, device):
         self.device = device
         self._test_conv_binary_base(dim=5)
@@ -700,6 +706,7 @@ class TestPatternMatcherGeneric(TestPatternMatcherBase):
 
 class TestPatternMatcher(TestPatternMatcherBase):
     @bf32_on_and_off()
+    @tf32_on_and_off()
     def test_linear_unary(self, device="cpu"):
         self.device = device
 
@@ -730,10 +737,12 @@ class TestPatternMatcher(TestPatternMatcherBase):
             dtypes.append(torch.bfloat16)
         if is_mkldnn_fp16_supported(self.device):
             dtypes.append(torch.float16)
-        if torch.backends.mkldnn.matmul.fp32_precision == "bf16":
+        if torch.backends.mkldnn.matmul.fp32_precision in ["bf16", "tf32"]:
             dtypes.append(torch.float32)
         options = itertools.product(unary_list, [True, False], dtypes)
         for unary_fn, bias, dtype in options:
+            if dtype != torch.float32 and torch.backends.mkldnn.matmul.fp32_precision == "tf32":
+                continue
             metrics.reset()
             mod = M(unary_fn, 10, 30, bias=bias).eval()
             # only fuse for linear when the dtype is bf16
@@ -762,6 +771,7 @@ class TestPatternMatcher(TestPatternMatcherBase):
             self.assertEqual(metrics.generated_kernel_count, expected_kernel_count)
 
     @bf32_on_and_off()
+    @tf32_on_and_off()
     @unittest.skipIf(not TEST_MKL, "Test requires MKL")
     def test_linear_fp32(self, device="cpu"):
         self.device = device
@@ -911,6 +921,7 @@ class TestPatternMatcher(TestPatternMatcherBase):
             self.assertEqual(metrics.generated_kernel_count, 3)
 
     @bf32_on_and_off()
+    @tf32_on_and_off()
     def test_linear_binary(self, device="cpu"):
         self.device = device
 
@@ -932,7 +943,7 @@ class TestPatternMatcher(TestPatternMatcherBase):
             dtypes.append(torch.bfloat16)
         if is_mkldnn_fp16_supported(self.device):
             dtypes.append(torch.float16)
-        if torch.backends.mkldnn.matmul.fp32_precision == "bf16":
+        if torch.backends.mkldnn.matmul.fp32_precision in ["bf16", "tf32"]:
             dtypes.append(torch.float32)
         options = itertools.product(
             binary_list, [[2, 3, 10], [2, 10]], [True, False], dtypes
@@ -941,7 +952,8 @@ class TestPatternMatcher(TestPatternMatcherBase):
 
         for binary_fn, input_shape, bias, dtype in options:
             metrics.reset()
-
+            if dtype != torch.float32 and torch.backends.mkldnn.matmul.fp32_precision == "tf32":
+                continue
             def matcher_check_fn():
                 self.assertEqual(
                     counters["inductor"][
